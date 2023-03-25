51232839864fef7e997c82def8dff7b0ee5788bd
Romnick Agsaoay <romnick.agsaoay@googlemail.com>
2023-03-25 14:10:22 +0000

    Hotel Listings API Repository Pattern implemented

--- HotelListing.API/Program.cs
+++ HotelListing.API/Program.cs
@@ -1,5 +1,7 @@
 using HotelListing.API.Configurations;
+using HotelListing.API.Contracts;
 using HotelListing.API.Data;
+using HotelListing.API.Repository;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
 
@@ -31,6 +33,10 @@ builder.Host.UseSerilog((ctx, lc) => lc.WriteTo.Console().ReadFrom.Configuration
 
 //add automapper
 builder.Services.AddAutoMapper(typeof(MapperConfig));
+
+//register repositories
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped<ICountriesRepository, CountriesRepository>();
 
 var app = builder.Build();
 
