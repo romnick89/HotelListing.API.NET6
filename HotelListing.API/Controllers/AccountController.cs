b63a940b28be6e86a642b346af3049779b704ce0
Romnick Agsaoay <romnick.agsaoay@googlemail.com>
2023-04-06 14:18:43 +0100

    Hotel Listings API Adding Logger

--- HotelListing.API/Controllers/AccountController.cs
+++ HotelListing.API/Controllers/AccountController.cs
@@ -10,10 +10,12 @@ namespace HotelListing.API.Controllers
     public class AccountController : ControllerBase
     {
         private readonly IAuthManager _authManager;
+        private readonly ILogger<AccountController> _logger;
 
-        public AccountController(IAuthManager authManager)
+        public AccountController(IAuthManager authManager, ILogger<AccountController> logger)
         {
             this._authManager = authManager;
+            this._logger = logger;
         }
         
         //POST: api/Account/regiser
@@ -24,17 +26,28 @@ namespace HotelListing.API.Controllers
         [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult> Register([FromBody]APIUserModel userModel)
         {
-            var errors = await _authManager.Register(userModel);
-
-            if (errors.Any())
+            _logger.LogInformation($"Registration Attempt for {userModel.Email}");
+            try
             {
-                foreach(var error in errors)
+                var errors = await _authManager.Register(userModel);
+
+                if (errors.Any())
                 {
-                    ModelState.AddModelError(error.Code, error.Description);
+                    foreach (var error in errors)
+                    {
+                        ModelState.AddModelError(error.Code, error.Description);
+                    }
+                    return BadRequest(ModelState);
                 }
-                return BadRequest(ModelState);
+                return Ok();
+
             }
-            return Ok();
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Something Went Wrong in the {nameof(Register)} - User Registration");
+                return Problem($"Something Went Wrong in the {nameof(Register)}. Please Contact Support", statusCode: 500); 
+            }
+            
         }
 
         //POST: api/Account/login
@@ -45,14 +58,25 @@ namespace HotelListing.API.Controllers
         [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult> Login([FromBody] LoginModel loginModel)
         {
-            var authResponse = await _authManager.Login(loginModel);
-
-            if (authResponse == null)
+            _logger.LogInformation($"Login Attempt for {loginModel.Email}");
+            try
             {
-                return Unauthorized();
-            }
+                var authResponse = await _authManager.Login(loginModel);
 
-            return Ok(authResponse);
+                if (authResponse == null)
+                {
+                    return Unauthorized();
+                }
+
+                return Ok(authResponse);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Something Went Wrong in the {nameof(Login)} - User Registration");
+                return Problem($"Something Went Wrong in the {nameof(Login)}. Please Contact Support", statusCode: 500);
+
+            }
+            
         }
 
         //POST: api/Account/refreshtoken
